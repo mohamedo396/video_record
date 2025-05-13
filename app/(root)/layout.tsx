d47e59ab7462af25fbd09118a574ec4a47aaa81a
mohamedo396 <mohamadamrani83@gmail.com>
2025-05-13 11:39:13 +0100

    Home Page UI

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/Navbar"
+import { ReactNode } from "react"
+
+
+const layout = ({children}:{children:ReactNode}) => {
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  )
+}
+
+export default layout
